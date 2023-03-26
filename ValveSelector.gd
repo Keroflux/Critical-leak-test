6cb881f35fadd799d1afab283dba010e5e21d5ad
Keroflux <49621236+Keroflux@users.noreply.github.com>
2023-03-26 22:15:24 +0200

    Finere ventil bokser

--- ValveSelector.gd
+++ ValveSelector.gd
@@ -13,7 +13,7 @@ func refresh(text):
 		if key.match("*" + text.to_upper() + "*"):
 			var a = button.instance()
 			$"%Valves".add_child(a)
-			a.get_child(0).text = key
+			a.get_node("%Tag").text = key
 			a.name = key
 			a.get_child(0).connect("pressed", get_parent(), "_set_valve", [key])
 			a.get_child(0).connect("pressed", self, "close")
